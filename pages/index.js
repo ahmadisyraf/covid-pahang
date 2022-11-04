3a1c4ea3fe06602e428e2f4c4be6efbc6405c2d9
ahmadisyraf <isyrafmagic@gmail.com>
2022-11-04 16:45:54 +0800

    new code

--- pages/index.js
+++ pages/index.js
@@ -8,153 +8,153 @@ import { Card, Title, AreaChart, BarChar, LineChart, Subtitle } from "@tremor/re
 const chartdata = [
   {
     date: "2022-10-01",
-    "cases_new": 29,
+    "new cases": 29,
   },
   {
     date: "2022-10-02",
-    "cases_new": 26,
+    "new cases": 26,
   },
   {
     date: "2022-10-03",
-    "cases_new": 19,
+    "new cases": 19,
   },
   {
     date: "2022-10-04",
-    "cases_new": 35,
+    "new cases": 35,
   },
   {
     date: "2022-10-05",
-    "cases_new": 25,
+    "new cases": 25,
   },
   {
     date: "2022-10-06",
-    "cases_new": 32,
+    "new cases": 32,
   },
   {
     date: "2022-10-07",
-    "cases_new": 26,
+    "new cases": 26,
   },
   {
     date: "2022-10-08",
-    "cases_new": 33,
+    "new cases": 33,
   },
   {
     date: "2022-10-09",
-    "cases_new": 16,
+    "new cases": 16,
   },
   {
     date: "2022-10-10",
-    "cases_new": 21,
+    "new cases": 21,
   },
   {
     date: "2022-10-11",
-    "cases_new": 18,
+    "new cases": 18,
   },
   {
     date: "2022-10-12",
-    "cases_new": 35,
+    "new cases": 35,
   },
   {
     date: "2022-10-13",
-    "cases_new": 40,
+    "new cases": 40,
   },
   {
     date: "2022-10-14",
-    "cases_new": 39,
+    "new cases": 39,
   },
   {
     date: "2022-10-15",
-    "cases_new": 50,
+    "new cases": 50,
   },
   {
     date: "2022-10-16",
-    "cases_new": 23,
+    "new cases": 23,
   },
   {
     date: "2022-10-17",
-    "cases_new": 17,
+    "new cases": 17,
   },
   {
     date: "2022-10-18",
-    "cases_new": 29,
+    "new cases": 29,
   },
   {
     date: "2022-10-19",
-    "cases_new": 42,
+    "new cases": 42,
   },
   {
     date: "2022-10-20",
-    "cases_new": 48,
+    "new cases": 48,
   },
   {
     date: "2022-10-21",
-    "cases_new": 46,
+    "new cases": 46,
   },
   {
     date: "2022-10-22",
-    "cases_new": 38,
+    "new cases": 38,
   },
   {
     date: "2022-10-23",
-    "cases_new": 30,
+    "new cases": 30,
   },
   {
     date: "2022-10-24",
-    "cases_new": 27,
+    "new cases": 27,
   },
   {
     date: "2022-10-25",
-    "cases_new": 18,
+    "new cases": 18,
   },
   {
     date: "2022-10-26",
-    "cases_new": 42,
+    "new cases": 42,
   },
   {
     date: "2022-10-27",
-    "cases_new": 68,
+    "new cases": 68,
   },
   {
     date: "2022-10-28",
-    "cases_new": 68,
+    "new cases": 68,
   },
   {
     date: "2022-10-29",
-    "cases_new": 51,
+    "new cases": 51,
   },
   {
     date: "2022-10-30",
-    "cases_new": 44,
+    "new cases": 44,
   },
   {
     date: "2022-10-31",
-    "cases_new": 43,
+    "new cases": 43,
   },
   {
     date: "2022-11-01",
-    "cases_new": 69,
+    "new cases": 69,
   },
   {
     date: "2022-11-02",
-    "cases_new": 79,
+    "new cases": 79,
   },
   {
     date: "2022-11-03",
-    "cases_new": 79,
+    "new cases": 79,
   }
 ];
 
 export default function Home() {
   return (
     <Card>
-      <Title>New COVID-19 Cases in Pahang (OCT - Nov)</Title>
+      <Title>New COVID-19 Cases in Pahang (Oct - Nov)</Title>
       <Subtitle>
         Data based on Malaysia Ministry of Health (MMOH)
       </Subtitle>
       <LineChart
         data={chartdata}
         dataKey="date"
-        categories={["cases_new"]}
+        categories={["new cases"]}
         colors={["blue"]}
         valueFormatter={undefined}
         marginTop="mt-6"
